853f52c75acf99df2555c91ca74700442dacf8c7
BricMichael <bricmichael@gmail.com>
2021-07-28 13:49:37 -0400

    #4. User Information Components.

--- src/components/widgetSm/WidgetSm.js
+++ src/components/widgetSm/WidgetSm.js
@@ -1,9 +1,93 @@
+import { Visibility } from '@material-ui/icons';
 import './widgetSm.css';
 
 const WidgetSm = () => {
     return (
         <div className='widgetSm'>
-        
+            <h3 className='widgetSmTitle'>New Join Members</h3>
+            <ul className='widgetSmList'>
+                <li className='widgetSmItem'>
+                    <img  
+                    src='https://www.euroresidentes.com/empresa/gestion/wp-content/uploads/sites/15/2007/10/empresarios-mediaticos.jpg' 
+                    alt='photoUser' 
+                    className='widgetSmImg' 
+                    />
+                    <div  className='widgetSmUser'>
+                        <span  className='widgetSmUserName'>Anna Keller</span>
+                        <span  className='widgetSmUserTitle'>
+                            Software Engineer
+                        </span>
+                    </div>
+                    <button className='widgetSmButton'>
+                            <Visibility />
+                            &nbsp; Display
+                    </button>
+                </li>
+                <li className='widgetSmItem'>
+                    <img  
+                    src='https://www.euroresidentes.com/empresa/gestion/wp-content/uploads/sites/15/2007/10/empresarios-mediaticos.jpg' 
+                    alt='photoUser' 
+                    className='widgetSmImg' />
+                    <div  className='widgetSmUser'>
+                        <span  className='widgetSmUserName'>Anna Keller</span>
+                        <span  className='widgetSmUserTitle'>
+                            Software Engineer
+                        </span>
+                    </div>
+                    <button className='widgetSmButton'>
+                            <Visibility />
+                            &nbsp; Display
+                    </button>
+                </li>
+                <li className='widgetSmItem'>
+                    <img  
+                    src='https://www.euroresidentes.com/empresa/gestion/wp-content/uploads/sites/15/2007/10/empresarios-mediaticos.jpg' 
+                    alt='photoUser' 
+                    className='widgetSmImg' />
+                    <div  className='widgetSmUser'>
+                        <span  className='widgetSmUserName'>Anna Keller</span>
+                        <span  className='widgetSmUserTitle'>
+                            Software Engineer
+                        </span>
+                    </div>
+                    <button className='widgetSmButton'>
+                            <Visibility />
+                            &nbsp; Display
+                    </button>
+                </li>
+                <li className='widgetSmItem'>
+                    <img  
+                    src='https://www.euroresidentes.com/empresa/gestion/wp-content/uploads/sites/15/2007/10/empresarios-mediaticos.jpg' 
+                    alt='photoUser' 
+                    className='widgetSmImg' />
+                    <div  className='widgetSmUser'>
+                        <span  className='widgetSmUserName'>Anna Keller</span>
+                        <span  className='widgetSmUserTitle'>
+                            Software Engineer
+                        </span>
+                    </div>
+                    <button className='widgetSmButton'>
+                            <Visibility />
+                            &nbsp; Display
+                    </button>
+                </li>
+                <li className='widgetSmItem'>
+                    <img  
+                    src='https://www.euroresidentes.com/empresa/gestion/wp-content/uploads/sites/15/2007/10/empresarios-mediaticos.jpg' 
+                    alt='photoUser' 
+                    className='widgetSmImg' />
+                    <div  className='widgetSmUser'>
+                        <span  className='widgetSmUserName'>Anna Keller</span>
+                        <span  className='widgetSmUserTitle'>
+                            Software Engineer
+                        </span>
+                    </div>
+                    <button className='widgetSmButton'>
+                            <Visibility />
+                            &nbsp; Display
+                    </button>
+                </li>       
+            </ul>
         </div>
     )
 }
